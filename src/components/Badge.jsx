ee91b7ac37cd60f277a96590c043e9fca6296d99
Solomon Njobvu <solomonnjobvu1@gmail.com>
2024-04-21 23:49:56 +0200

    animation with framer motion finished demo

---
+++ src/components/Badge.jsx
@@ -0,0 +1,9 @@
+import { motion } from "framer-motion";
+
+export default function Badge({ caption }) {
+  return (
+    <motion.span animate={{ scale: [1, 1.2, 1] }} transition={{duration: .3}} className="badge">
+      {caption}
+    </motion.span>
+  );
+}
---
+++ src/components/ChallengeItem.jsx
@@ -0,0 +1,76 @@
+import { useContext } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+import { ChallengesContext } from "../store/challenges-context.jsx";
+
+export default function ChallengeItem({
+  challenge,
+  onViewDetails,
+  isExpanded,
+}) {
+  const { updateChallengeStatus } = useContext(ChallengesContext);
+
+  const formattedDate = new Date(challenge.deadline).toLocaleDateString(
+    "en-US",
+    {
+      day: "2-digit",
+      month: "short",
+      year: "numeric",
+    }
+  );
+
+  function handleCancel() {
+    updateChallengeStatus(challenge.id, "failed");
+  }
+
+  function handleComplete() {
+    updateChallengeStatus(challenge.id, "completed");
+  }
+
+  return (
+    <motion.li layout>
+      <article className="challenge-item">
+        <header>
+          <img {...challenge.image} />
+          <div className="challenge-item-meta">
+            <h2>{challenge.title}</h2>
+            <p>Complete until {formattedDate}</p>
+            <p className="challenge-item-actions">
+              <button onClick={handleCancel} className="btn-negative">
+                Mark as failed
+              </button>
+              <button onClick={handleComplete}>Mark as completed</button>
+            </p>
+          </div>
+        </header>
+        <div className={`challenge-item-details}`}>
+          <p>
+            <button onClick={onViewDetails}>
+              View Details{" "}
+              <motion.span
+                animate={{ rotate: isExpanded ? 180 : 0 }}
+                transition={{ duration: 0.5 }}
+                className={`challenge-item-details-icon`}
+              >
+                &#9650;
+              </motion.span>
+            </button>
+          </p>
+          <AnimatePresence>
+            {isExpanded && (
+              <motion.div
+                initial={{ opacity: 0, y: -20 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -20 }}
+              >
+                <p className="challenge-item-description">
+                  {challenge.description}
+                </p>
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </div>
+      </article>
+    </motion.li>
+  );
+}
